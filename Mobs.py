2a29abe544fc95a46526b5e5159e623f535e9889
Paul7772 <123050309+Paul7772@users.noreply.github.com>
2024-03-14 10:43:16 +0500

    Update Mobs.py

--- Mobs.py
+++ Mobs.py
@@ -1,42 +1,37 @@
 import pygame
+from random import randint
 
 pygame.init()
 
 W = 1324
 
 
-class Zombie(pygame.sprite.Sprite):
-    def __init__(self, x: int, y: int):
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y, path, weight, height, speed, damage, hp):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('Sprite/Game/Zombie/Zombie.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (94, 104))
+        self.image = pygame.image.load(path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (weight, height))
         self.rect = self.image.get_rect(center=(x, y))
-        self.lvl = 1
-        self.damage = self.lvl * 2
-        self.hp = self.lvl * 20
-        self.speed = 1
-        self.can_attack = True
-        self.attack_time = None
-        self.attack_duration_cooldown = 2000
-
-    def attack(self, player):
-        if self.can_attack:
-            self.can_attack = False
-            self.attack_time = pygame.time.get_ticks()
-            player.heart -= self.damage
-            if player.heart <= 0:
-                player.kill()
-
-    def cooldown(self):
-        current_time = pygame.time.get_ticks()
-        if not self.can_attack:
-            if current_time - self.attack_time >= self.attack_duration_cooldown:
-                self.can_attack = True
+        self.damage = damage
+        self.hp = hp
+        self.speed = speed
 
     def update(self):
-        self.cooldown()
         if self.rect.x >= W + 25:
             self.kill()
         if self.hp <= 0:
             self.kill()
         self.rect.x += self.speed
+
+
+class Zombie(Enemy):
+    def __init__(self):
+        super().__init__(10, randint(20, 850), 'Sprite/Game/Enemy/Zombie.png', 94,
+                         104, 1, 2, 20)
+
+
+class Robbers(Enemy):
+    def __init__(self):
+        super().__init__(10, randint(20, 850), 'Sprite/Game/Enemy/robber.png', 94,
+                         104, 1, 2, 20)
+
