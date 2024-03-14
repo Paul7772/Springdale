85226a39d9ff7f923778f2b75d30a97d7708f4d8
Paul7772 <pmikhanov2010@mail.ru>
2024-03-15 00:07:52 +0500

    fix

--- Mobs.py
+++ Mobs.py
@@ -7,15 +7,9 @@ W = 1324
 
 
 class Enemy(pygame.sprite.Sprite):
-<<<<<<< HEAD
     def __init__(self, x, y, image, weight, height, speed, damage, hp):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(f'Sprite/Game/Enemy/{image}').convert_alpha()
-=======
-    def __init__(self, x, y, path, weight, height, speed, damage, hp):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(path).convert_alpha()
->>>>>>> fc9b3ec3c353372b3ec2482b1b0f71acc087ed98
         self.image = pygame.transform.scale(self.image, (weight, height))
         self.rect = self.image.get_rect(center=(x, y))
         self.damage = damage
@@ -32,7 +26,6 @@ class Enemy(pygame.sprite.Sprite):
 
 class Zombie(Enemy):
     def __init__(self):
-<<<<<<< HEAD
         super().__init__(10, randint(20, 850), 'Zombie.png', 48,
                          84, 1, 2, 20)
 
@@ -41,14 +34,4 @@ class Robber(Enemy):
     def __init__(self):
         super().__init__(10, randint(20, 850), 'robber.png', 48,
                          84, 3, 5, 10)
-=======
-        super().__init__(10, randint(20, 850), 'Sprite/Game/Enemy/Zombie.png', 94,
-                         104, 1, 2, 20)
 
-
-class Robbers(Enemy):
-    def __init__(self):
-        super().__init__(10, randint(20, 850), 'Sprite/Game/Enemy/robber.png', 94,
-                         104, 1, 2, 20)
-
->>>>>>> fc9b3ec3c353372b3ec2482b1b0f71acc087ed98
--- Settings.py
+++ Settings.py
@@ -1,4 +1,3 @@
-
 import pygame
 
 W, H = 1324, 900
@@ -46,9 +45,3 @@ def create_object(classes, all_sprite, group):
 
 list_weapon = ['sword', 'bow']
 
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> fc9b3ec3c353372b3ec2482b1b0f71acc087ed98
