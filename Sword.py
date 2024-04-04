7242ff9870dca7998af447c8a3d123fd9e1aefd4
Paul7772 <123050309+Paul7772@users.noreply.github.com>
2024-04-04 10:33:07 +0500

    Update Sword.py

--- Sword.py
+++ Sword.py
@@ -4,29 +4,30 @@ pygame.init()
 
 
 class Sword(pygame.sprite.Sprite):
-    def __init__(self, x: int, y: int, see):
+    def __init__(self, pos: tuple, see, group):
         pygame.sprite.Sprite.__init__(self)
+        super().__init__(group)
         self.list_image = ['Sprite/Game/Sword/Sword_left_right.png', 'Sprite/Game/Sword/Sword_up_down.png']
         self.level = 1
         self.damage = self.level * 10
         if see == 'left':
             self.image = pygame.image.load(self.list_image[0]).convert_alpha()
             self.image = pygame.transform.scale(self.image, (64, 64))
-            self.rect = self.image.get_rect(midright=(x, y))
+            self.rect = self.image.get_rect(midright=pos)
         elif see == 'right':
             self.image = pygame.image.load(self.list_image[0]).convert_alpha()
             self.image = pygame.transform.scale(self.image, (64, 64))
             self.image = pygame.transform.flip(self.image, True, False)
-            self.rect = self.rect = self.image.get_rect(midleft=(x, y))
+            self.rect = self.rect = self.image.get_rect(midleft=pos)
         elif see == 'down':
             self.image = pygame.image.load(self.list_image[1]).convert_alpha()
             self.image = pygame.transform.scale(self.image, (64, 64))
-            self.rect = self.image.get_rect(midtop=(x, y))
+            self.rect = self.image.get_rect(midtop=pos)
         elif see == 'up':
             self.image = pygame.image.load(self.list_image[1]).convert_alpha()
             self.image = pygame.transform.scale(self.image, (64, 64))
             self.image = pygame.transform.flip(self.image, False, True)
-            self.rect = self.rect = self.image.get_rect(midbottom=(x, y))
+            self.rect = self.rect = self.image.get_rect(midbottom=pos)
         self.can_attack = True
         self.attack_time = None
 
