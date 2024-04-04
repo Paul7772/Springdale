cf53007e0203cc5053f359059aa080de3fe51015
Paul7772 <123050309+Paul7772@users.noreply.github.com>
2024-04-04 10:32:31 +0500

    Update level.py

--- level.py
+++ level.py
@@ -8,13 +8,17 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group
+        self.enemy_sprites = pygame.sprite.Group()
 
-    def create_object(self):
-        player = Player((100, 100), None, None, None, None)
-        self.visible_sprites.add(player)
+        self.create_objects()
+
+    def create_objects(self):
+        self.player = Player((1200, 830), [self.visible_sprites], self.enemy_sprites)
+
+    def create_attack(self):
+        pass
 
     def run(self):
         # update and draw sprite
-        self.create_object()
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
